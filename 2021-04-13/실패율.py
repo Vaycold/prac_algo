5fbb8dc3666a91b4a02e58f7c836778df8930018
godbang <moch1996@naver.com>
2021-04-13 22:30:58 +0900

    Create 실패율.py
    2019 KAKAO BLIND RECRUITMENT

---
+++ 2021-04-13/실패율.py
@@ -0,0 +1,12 @@
+def solution(N, stages):
+    result = {}
+    length = len(stages)              #총 사람의 수
+    for stage in range(1, N+1) :
+        if length == 0 :
+            result[stage] = 0
+        else :
+            ct = stages.count(stage) # 그 'stage' 머무르게 된 사람의 수
+            result[stage] = ct / length
+            length -= ct
+    result = sorted(result, key = lambda x : result[x], reverse=True)        
+    return result
