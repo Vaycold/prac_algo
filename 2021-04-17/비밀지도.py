40404f5a893717d12caf36e340e4e7c6d16d4383
godbang <moch1996@naver.com>
2021-04-17 00:47:42 +0900

    Delete 비밀지도.py

--- 2021-04-17/비밀지도.py
+++
@@ -1,9 +0,0 @@
-def solution(n, arr1, arr2):
-    answer = []
-    for i, j in zip(arr1, arr2) :
-        # 비트연산자'|' 사용 or의미임.
-        # rjust는 n칸만큼 할당해서 우측으로 밀착시키는 개념
-        #  - 나머지는 0으로 채움.
-        result = str(bin(i|j))[2:].rjust(n,'0').replace('1','#').replace('0',' ')
-        answer.append(result)
-    return answer
