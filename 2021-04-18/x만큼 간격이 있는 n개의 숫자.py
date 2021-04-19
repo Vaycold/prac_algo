2a7dffb016a733e1fdfb1ccf7d362c2d39fe9130
godbang <moch1996@naver.com>
2021-04-20 03:55:28 +0900

    Create py

---
+++ 2021-04-18/x만큼 간격이 있는 n개의 숫자.py
@@ -0,0 +1,11 @@
+def solution(x, n):
+    answer = []
+    ans = 0
+    for i in range(n) :
+        ans += x 
+        answer.append(ans)
+    
+    return answer
+'''
+이번문제도 그렇게 어렵지는 않았다.
+'''
--- 2021-04-19/직사각형 별찍기.py
+++
@@ -1,6 +0,0 @@
-a, b = map(int, input().strip().split(' '))
-for i in range(b) :
-    print('*'*a)
-'''
-이번 문제는 많이 쉬웠던 것 같다.
-'''
